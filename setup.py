d018d5b525d2adb8dc57388a34a371f76980edb4
lighink <wmnh1@163.com>
2021-11-08 12:56:51 +0900

    add the setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='GaPP3',
+      version='3.0',
+      description='Gaussian Processes in Python3',
+      packages=['gapp', 'gapp.covfunctions'],
+     )
